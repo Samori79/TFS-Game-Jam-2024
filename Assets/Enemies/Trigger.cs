dafe4c9e6f971949374f03de4d7a093da5de28c9
RosemaryBagels <148225175+RosemaryBagels@users.noreply.github.com>
2024-04-03 15:04:36 -0400

    Update Trigger.cs

--- Assets/Enemies/Trigger.cs
+++ Assets/Enemies/Trigger.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class Trigger : MonoBehaviour
 {
     bool toggle = false;
-    
+    Transform playerRef;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRef = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("E"))
+        {
+
+        }
     }
 }
