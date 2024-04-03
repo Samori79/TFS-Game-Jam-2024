92491046af9df92fde10a28e16bfd04d32bfaa91
Lvl100Wizard <jsands180@gmail.com>
2024-04-02 21:57:25 -0400

    Animations + Player Movement
    Added most of the animations to the player for idle and moving WASD

--- Assets/PlayerTestFolder/PlayerMovement.cs
+++ Assets/PlayerTestFolder/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
+    public Animator animator;
 
     Vector2 movement;
 
@@ -16,13 +17,15 @@ public class PlayerMovement : MonoBehaviour
        movement.x = Input.GetAxisRaw("Horizontal");
        movement.y = Input.GetAxisRaw("Vertical");
 
+       animator.SetFloat("Horizontal", movement.x);
+       animator.SetFloat("Vertical", movement.y);
+       animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
     {
     //movement
-    rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
-
+    rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
 
 
     }
