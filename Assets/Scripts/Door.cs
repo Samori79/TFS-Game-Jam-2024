01abfe1da235849bacbfe359303bd3ed8ed501a5
Lvl100Wizard <jsands180@gmail.com>
2024-04-04 00:40:03 -0400

    DOORS KEYS AND LOCKS, OH MY

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,13 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class Door : MonoBehaviour
 {
+
+    private bool active = false;
+
+    public PlayableDirector pd;
     // Start is called before the first frame update
     void Start()
     {
-        
+        keyscript.Unlock += Activate;
+
+        pd = GetComponent<PlayableDirector>();
+
+
     }
 
     // Update is called once per frame
@@ -15,4 +25,13 @@ public class Door : MonoBehaviour
     {
         
     }
+
+public void Activate()
+    {
+        active = !active;
+        pd.Play();
+        Debug.Log("door state changed");
+    }
+
+
 }
